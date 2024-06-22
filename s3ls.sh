7e52e5cd1822d2c901a5b6151b6ce593386519e5
allthingsross <all.things.ross@proton.me>
2024-06-22 14:48:44 +0100

    Prettify the output by adding emtpy line after each s3 bucket listing

--- s3ls.sh
+++ s3ls.sh
@@ -6,4 +6,5 @@ aws s3 ls | awk '{ print $NF }' | while read BUCKET
 do 
   echo -e "s3://${BUCKET}:"
   aws s3 ls  --recursive s3://${BUCKET} | gawk -v bucket=${BUCKET} '{ print $1,$2,$3,"s3://"bucket"/"$NF }'
+  echo -e ""
 done
